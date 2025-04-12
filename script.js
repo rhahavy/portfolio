e3f0b5024ead4ef35aab88a5f1cf3b6d70e97f5c
rhahavy <rhahavy.b@gmail.com>
2025-04-12 09:48:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 console.log("Hello from your script.js file!");
+const toggle = document.getElementById("themeToggle");
+
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+});
